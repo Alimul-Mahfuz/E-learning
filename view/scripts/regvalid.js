f7b593a6743beac86f7ea3d265cc95ead0894daa
Alimul-Mahfuz <74812296+Alimul-Mahfuz@users.noreply.github.com>
2022-04-07 10:24:28 +0600

    validation implementation successful
    Javascript validation added to singup.php file. This will validate the input and match the phone number with regular expression.

---
+++ view/scripts/regvalid.js
@@ -0,0 +1,170 @@
+//The following code will validate the inputs enterd by user in singup.php
+
+// alert("Hello");
+// function validate() {
+//     var pregx = /^(?:(?:\+|00)88|01)?\d{11}$/; //This line is used as regular expression to validate phone number
+//     var fname = document.getElementById('fname').value;
+//     var lname = document.getElementById('lname').value;
+//     console.log(fname);
+//     console.log(lname);
+//     var email = document.forms['reg-form']['email'].value;
+//     var phone = document.forms['reg-form']['phone'].value;
+//     var password = document.forms['reg-form']['password'].value;
+//     var repassword = document.forms['reg-form']['re-password'].value;
+//     if (fname == '') {
+//         alert("Hello Tushar");
+//         var ferr = document.getElementById('fnameErr');
+//         ferr.style.display = 'block';
+//         ferr.style.color = 'red';
+//         return false;
+//     }
+//     // if (lname == '') {
+//     //     alert("lname empty");
+//     //     var lerr = document.getElementById('lnameErr');
+//     //     lerr.style.display = 'visible';
+//     //     lerr.style.color = 'red';
+//     //     return false;
+//     // }
+
+//     // if (email == '') {
+//     //     var eerr = document.getElementById('emailErr');
+//     //     eerr.style.display = 'block';
+//     //     eerr.style.color = 'red';
+//     //     console.log("Email error");
+//     //     return false;
+//     // }
+//     // if (phone == '') {
+//     //     var perr = document.getElementById('phoneErr');
+//     //     perr.style.display = 'block';
+//     //     perr.style.color = 'red';
+//     //     return false;
+//     // }
+//     // // Phone number validation using regular expression
+//     // if (phone != '' && !phone.match(pregx)) {
+//     //     var perr = document.getElementById('phoneErr');
+//     //     perr.innerHTML = "This isn't phone number";
+//     //     perr.style.display = 'block';
+//     //     perr.style.color = 'red';
+//     //     return false;
+//     // }
+//     // if (password == '') {
+//     //     var paerr = document.getElementById('passErr');
+//     //     paerr.innerHTML = "Password cannot be empty";
+//     //     // perr.innerHTML = "This isn't phone number";
+//     //     paerr.style.display = 'block';
+//     //     paerr.style.color = 'red';
+//     //     console.log('hello');
+//     //     return false;
+//     // }
+
+
+// }
+
+
+
+function validate() {
+    //errFlag track the error of the input field and prevent the submission of the from
+    //by returning false
+    var errFlag = false;
+
+    //Regular expression string to validate phone number
+    var pregx = /^(?:(?:\+|00)88|01)?\d{11}$/;
+    //Function to display error message
+    function showError(id) {
+        id.style.color = 'red';
+        id.style.display = 'block';
+        errFlag = true;
+    }
+    var fname = document.getElementById('fname');
+    var lname = document.getElementById('lname');
+    var email = document.getElementById('email');
+    var phone = document.getElementById('phone');
+    var password = document.getElementById('password');
+    var rePass = document.getElementById('rePass');
+    var urole = document.getElementById('urole');
+    if (fname.value === '') {
+        let fid = document.getElementById('fnameErr');
+        showError(fid);
+    }
+    if (fname.value != '') {
+        let fid = document.getElementById('fnameErr');
+        fid.style.display = 'none';
+    }
+    if (lname.value === '') {
+        let fid = document.getElementById('lnameErr');
+        showError(fid);
+    }
+    if (lname.value != '') {
+        let fid = document.getElementById('fnameErr');
+        fid.style.display = 'none';
+        console.log(lname.value);
+    }
+    if (email.value === '') {
+        let fid = document.getElementById('emailErr');
+        showError(fid);
+    }
+    if (email.value != '') {
+        let fid = document.getElementById('emailErr');
+        fid.style.display = 'none';
+    }
+    if (phone.value === '') {
+        let fid = document.getElementById('phoneErr');
+        showError(fid);
+    }
+    if (phone.value != '') {
+        if (!phone.value.match(pregx)) {
+            let peid = document.getElementById('phoneErr');
+            peid.style.color = 'red';
+            peid.style.display = 'block';
+            peid.textContent = 'Enter a valid Bangladeshi phone number in 11 digit';
+            errFlag = true;
+        }
+        else {
+            let fid = document.getElementById('phoneErr');
+            fid.style.display = 'none';
+        }
+    }
+    if (password.value == '') {
+        let fid = document.getElementById('passErr');
+        showError(fid);
+    }
+    if (password.value != '') {
+        let fid = document.getElementById('passErr');
+        fid.style.display = 'none';
+    }
+    if (rePass.value == '') {
+        let fid = document.getElementById('repassErr');
+        showError(fid);
+
+    }
+    if (rePass.value != '') {
+        let peid = document.getElementById('repassErr');
+        if (!rePass.value.match(password.value)) {
+            peid.style.color = 'red';
+            peid.style.display = 'block';
+            peid.textContent = 'Passwrod did not matched with retyped password!';
+            errFlag = true;
+        }
+        else {
+            peid.style.display = 'none';
+        }
+    }
+    if (urole.value == '') {
+        let fid = document.getElementById('roleErr');
+        showError(fid);
+    }
+    if (urole.value != '') {
+        let fid = document.getElementById('roleErr');
+        fid.style.display = 'none';
+    }
+    console.log(urole.value);
+    console.log(errFlag);
+
+    if (errFlag) {
+        return false;
+    }
+    if (!errFlag) {
+        i
+        return true;
+    }
+}
