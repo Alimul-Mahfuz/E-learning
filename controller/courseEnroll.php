b00c3fb40d8795fb330bafabbfc194fa7c7b4213
Alimul-Mahfuz <74812296+Alimul-Mahfuz@users.noreply.github.com>
2022-04-15 15:42:21 +0600

    Duplicate enroll fixed

--- controller/courseEnroll.php
+++ controller/courseEnroll.php
@@ -10,6 +10,7 @@ $conn = new mysqli(
     $password,
     $dbname
 );
+$csa = array();
 
 if ($conn->connect_errno) {
     echo 'Connect error';
@@ -23,11 +24,21 @@ if ($conn->connect_errno) {
             $id = $row['user_id'];
         }
     }
-    $dnow = date('y-m-d');
-    $enrolqry = "INSERT INTO payment (user_id,c_id,date) VALUES ('" . $id . "','" . $_GET['cid'] . "','" . $dnow . "')";
-    if ($conn->query($enrolqry)) {
-        header("location:../view/student/student-dashboard.php");
-    } else {
-        echo "faild to insert";
+    $check= "Select pay_id from payment where user_id='".$id."' and c_id='".$_GET['cid']."'";
+    $chkres = $conn->query($check);
+    if($chkres->num_rows>0){
+
+        echo "You are already enrooled this course";
+        header( "refresh:2;url=../view/student/student-dashboard.php" );
     }
+    else{
+        $dnow = date('y-m-d');
+        $enrolqry = "INSERT INTO payment (user_id,c_id,date) VALUES ('" . $id . "','" . $_GET['cid'] . "','" . $dnow . "')";
+        if ($conn->query($enrolqry)) {
+            header("location:../view/student/student-dashboard.php");
+        } else {
+            echo "faild to insert";
+        }
+    }
+
 }
